9163809784047737e18faf189047ebf1acbdc92d
Dio Seung Heo <elondhs@gmail.com>
2020-01-17 18:02:43 +0900

    add custom attr

--- library/src/main/java/com/buzzvil/labeledfab/LabeledFloatingActionButton.kt
+++ library/src/main/java/com/buzzvil/labeledfab/LabeledFloatingActionButton.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.Gravity
@@ -18,26 +19,50 @@ import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class LabeledFloatingActionButton : FloatingActionButton{
+class LabeledFloatingActionButton : FloatingActionButton {
     constructor(ctx: Context) : super(ctx) {
-        init()
+        init(null)
     }
 
     constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs) {
-        init()
+        init(attrs)
     }
 
     constructor(ctx: Context, attrs: AttributeSet, defStyleAttr: Int) : super(ctx, attrs, defStyleAttr) {
-        init()
+        init(attrs)
     }
 
-    private fun init() {
-        setImageDrawable(createFABDrawableWithText(android.R.drawable.ic_dialog_email, "Label", 15f, Color.CYAN))
+    private fun init(attrs: AttributeSet?) {
+        var text: String?
+        var textSizePx: Int
+        var textColor: Int
+        var icon: Int
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.LabeledFloatingActionButton,
+            0, 0).apply {
+
+            try {
+                text = getString(R.styleable.LabeledFloatingActionButton_labeledFabText)
+                textSizePx = getDimensionPixelSize(R.styleable.LabeledFloatingActionButton_labeledFabTextSize, 0)
+                textColor = getColor(R.styleable.LabeledFloatingActionButton_labeledFabTextColor, Color.BLACK)
+                icon = getResourceId(R.styleable.LabeledFloatingActionButton_labeledFabIcon, 0)
+            } finally {
+                recycle()
+            }
+        }
+
+        if (!TextUtils.isEmpty(text) && textSizePx > 0 && icon > 0) {
+            scaleType = ScaleType.FIT_CENTER
+            setImageDrawable(createFABDrawableWithText(icon, text!!, textSizePx, textColor))
+        } else if (icon > 0) {
+            setImageResource(icon)
+        }
     }
 
     private fun createFABDrawableWithText(
         @DrawableRes iconResId: Int, text: String,
-        textSizeDp: Float, @ColorInt textColor: Int
+        textSizePx: Int, @ColorInt textColor: Int
     ): Drawable? {
         val linearLayout = LinearLayout(context).apply {
             layoutParams = LinearLayout.LayoutParams(
@@ -54,10 +79,9 @@ class LabeledFloatingActionButton : FloatingActionButton{
 
         val textView = TextView(context).apply {
             setTextColor(textColor)
-            setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSizeDp)
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizePx.toFloat())
             setText(text)
             gravity = Gravity.CENTER
-            typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
             setPadding(0, 0, 0, 0)
         }
 
